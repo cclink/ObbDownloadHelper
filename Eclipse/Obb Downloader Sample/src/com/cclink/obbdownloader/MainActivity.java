56b41594c8b0fbcfbcc919a224ac2035e94e65d1
zjn0645 <zhome90@163.com>
2016-04-11 16:52:02 +0800

    add sample project

---
+++ Eclipse/Obb Downloader Sample/src/com/cclink/obbdownloader/MainActivity.java
@@ -0,0 +1,51 @@
+package com.cclink.obbdownloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private ObbDownloadHelper mDownloaderHelper;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mDownloaderHelper = new ObbDownloadHelper(this);
+		if (!mDownloaderHelper.expansionFilesDelivered()) {
+			mDownloaderHelper.downloadExpansionFiles(this, new ObbDownloadHelper.onDownloadStateChanged() {
+				@Override
+				public void onDownloadSuccess() {
+					Toast.makeText(MainActivity.this, "Download success.", Toast.LENGTH_SHORT).show();
+				}
+				
+				@Override
+				public void onDownloadFailed() {
+					Toast.makeText(MainActivity.this, "Download failed.", Toast.LENGTH_SHORT).show();
+				}
+				
+				@Override
+				public void onDownloadCanceled() {
+					Toast.makeText(MainActivity.this, "Download canceled.", Toast.LENGTH_SHORT).show();
+				}
+			});
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (mDownloaderHelper != null) {
+			mDownloaderHelper.onResume();
+		}
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (mDownloaderHelper != null) {
+			mDownloaderHelper.onStop();
+		}
+	}
+}
