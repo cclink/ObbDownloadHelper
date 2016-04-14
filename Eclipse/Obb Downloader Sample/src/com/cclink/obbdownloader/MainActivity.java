3b8b9f736fa86140b880bfd522b6e33e66f6c689
zjn0645 <zhome90@163.com>
2016-04-14 19:26:57 +0800

    modify the mainactivity to set the obbinfo by parameter

--- Eclipse/Obb Downloader Sample/src/com/cclink/obbdownloader/MainActivity.java
+++ Eclipse/Obb Downloader Sample/src/com/cclink/obbdownloader/MainActivity.java
@@ -12,7 +12,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mDownloaderHelper = new ObbDownloadHelper(this);
+		mDownloaderHelper = new ObbDownloadHelper(this, new ObbInfo() {
+			
+			@Override
+			public String getPublicKey() {
+				return "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp2hAfnmkYIs11L9w0GFOQjEyj38Ce+59L3ECDn2AuWenQiangXu3CzpOkZP8VlMe1ZwyEtsqI4VH8L1wIS100DDhzQKByy6YzADFiG0eU4o5uMDLBwlpG7KZXBTf2r881AyPfUYASwrdrNR+EzaXNYmYNsDTYmSAGtpyIqSZMc0gXKjTGF9DPcdVgnGQO1fAMgczeOQI2+gMDT1ynfMF5U72iqmf8D6SA4O6U8f4r1MfGvsujSOeGvitxjL05n50iq0J0CGMkZOZofTBT68kFiUv1oD6kvQODuBM3juYWKnnq4wBM/oBDYYBi+X3zuoScCw+kizmn1XXfNNDXkBVLwIDAQAB";
+			}
+			
+			@Override
+			public int getMainObbVersion() {
+				return 3;
+			}
+			
+			@Override
+			public long getMainObbFileSize() {
+				return 1546530L;
+			}
+		});
 		if (!mDownloaderHelper.expansionFilesDelivered()) {
 			mDownloaderHelper.downloadExpansionFiles(this, new ObbDownloadListener() {
 				@Override
