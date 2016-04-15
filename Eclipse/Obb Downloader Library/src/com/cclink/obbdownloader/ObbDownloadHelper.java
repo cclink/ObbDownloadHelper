806f466cf5782a5e99c955ebc03347631020e617
zjn0645 <zhome90@163.com>
2016-04-15 13:57:40 +0800

    change XAPKFile as a non public class

--- Eclipse/Obb Downloader Library/src/com/cclink/obbdownloader/ObbDownloadHelper.java
+++ Eclipse/Obb Downloader Library/src/com/cclink/obbdownloader/ObbDownloadHelper.java
@@ -21,24 +21,6 @@ import android.view.WindowManager;
 
 public class ObbDownloadHelper implements IDownloaderClient {
 
-    /**
-     * This is a little helper class that demonstrates simple testing of an
-     * Expansion APK file delivered by Market. You may not wish to hard-code
-     * things such as file lengths into your executable... and you may wish to
-     * turn this code off during application development.
-     */
-    private static class XAPKFile {
-        public final boolean mIsMain;
-        public final int mFileVersion;
-        public final long mFileSize;
-
-        XAPKFile(boolean isMain, int fileVersion, long fileSize) {
-            mIsMain = isMain;
-            mFileVersion = fileVersion;
-            mFileSize = fileSize;
-        }
-    }
-
     private Context mContext;
     private ObbDownloadListener mListener;
     private IStub mDownloaderClientStub;
