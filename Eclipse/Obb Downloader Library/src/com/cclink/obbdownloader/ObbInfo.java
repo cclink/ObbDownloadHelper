fc30dfad117bb0c870648a57881bc43067d27445
zjn0645 <zhome90@163.com>
2016-04-18 11:06:30 +0800

    add some comments for getSalt() method

--- Eclipse/Obb Downloader Library/src/com/cclink/obbdownloader/ObbInfo.java
+++ Eclipse/Obb Downloader Library/src/com/cclink/obbdownloader/ObbInfo.java
@@ -8,14 +8,18 @@ public abstract class ObbInfo {
     // It is used by Google Play Licensing Library to verify the identity of the client.
     public abstract String getPublicKey();
     
+    // The salt is used by PreferenceObfuscator class to obfuscate the value stored in SharedPreferences
+    // It only makes sense in local storage, and would not be sent to the network
     public byte[] getSalt() {
     	return SALT;
     }
     
-    // you must have main obb in your application 
+    // You use this library, we assume that you must have main obb file in your application
+    // So you should override getMainObbVersion() and getMainObbFileSize() to give a correct obb file version and size.
     public abstract int getMainObbVersion();
     public abstract long getMainObbFileSize();
     
+    // Override getPatchObbVersion() and getPatchObbFileSize() only when you have patch obb file.
     public int getPatchObbVersion() {
     	return 0;
     }
